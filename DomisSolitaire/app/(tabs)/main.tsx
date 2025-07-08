aafb824936c4277e4bf51200dafbe52c959b0372
Domistreng <Domisrotmg@gmail.com>
2025-07-07 21:30:03 -0400

    main.tsx imported from insp

---
+++ DomisSolitaire/app/(tabs)/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    StatusBar,
+} from 'react-native';
+
+import Board from './components/board';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle='default' />
+                <Board />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
